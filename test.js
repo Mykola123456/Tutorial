b1366f46a69f8d74662d688e5f6a3f4294fecd17
Mykola123456 <125387635+Mykola123456@users.noreply.github.com>
2023-05-17 21:46:37 +0300

    1

--- test.js
+++ test.js
@@ -1,14 +1,16 @@
-const apartment = {
-  imgUrl: "https://via.placeholder.com/640x480",
-  descr: "Spacious apartment in the city center",
-  rating: 4,
-  price: 2153,
-  tags: ["premium", "promoted", "top"],
-  owner: {
-   name: "Henry",
-    phone: "982-126-1588",
-   email: "henry.carter@aptmail.com", 
-    
-    }
-  console.log(apartment)
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  public: true,
+  rating: 8.38,
 };
+
+// const bookTitle = book.title;
+// console.log(bookTitle); // "The Last Kingdom"
+
+// const bookGenres = book.genres;
+// console.log(bookGenres); // ["historical prose", "adventure"]
+
+// const bookPrice = book.price;
+// console.log(bookPrice); // undefined
